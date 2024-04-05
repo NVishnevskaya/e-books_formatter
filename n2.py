af358403aa0c9a1690b819b52d45e025d4ecd312
Ralathor-38B <141573057+Ralathor-38B@users.noreply.github.com>
2024-04-06 00:29:46 +0300

    upgraded function

--- n2.py
+++ n2.py
@@ -2,51 +2,60 @@ import xml.etree.ElementTree as ET
 
 from flask import Flask
 
-d = {'title': 'h1', 'empty-line': 'br'}
-a = {'epigraph', 'poem', 'stanza', 'v', 'text-author'}
-
 
 def fb2_parser(name):
+    change = {'title': 'h1', 'empty-line': 'p'}
+    a = {'epigraph', 'poem', 'stanza', 'v', 'text-author'}
     tree = ET.parse(name)
     root = tree.getroot()
     for element in root.iter():
         element.tag = element.tag.partition('}')[-1]
 
+    # region create a dict of metadata
     description = dict()
     r = root.find('./description')
     for el in r.iter():
-        if not el.text is None and not el.text.isspace():
-            s = ' '.join(el.text.split('\n'))
+        if not (el.text is None) and not el.text.isspace():
+            text = ' '.join(el.text.split('\n'))
             if el.tag in description:
-                description[el.tag] += f'\n{s}'
+                description[el.tag] += f'\n{text}'
             else:
-                description[el.tag] = s
+                description[el.tag] = text
+    # endregion
 
-    bi = dict()
+    # region create a dict of binary data of images
+    images = dict()
     for elem in root.findall('binary'):
         i = elem.attrib
-        bi[i['id']] = [i.get('content-type', 'png').split('/')[-1], elem.text]
+        images[i['id']] = [i.get('content-type', 'png').split('/')[-1], elem.text]
+    # endregion
 
-    for s in root.iter('image'):
-        v = s.attrib
+    # region add attribute of bin images and change tag (for html)
+    for img in root.iter('image'):
+        v = img.attrib
         for i in v.keys():
             if 'href' in i:
                 id = v[i][1:]
-                form, c = bi.get(id)
-                s.set('scr', f'data:image/{form};base64,{c}')
-                s.tag = 'img'
+                form, c = images.get(id)
+                img.set('scr', f'data:image/{form};base64,{c}')
+                img.tag = 'img'
                 break
-    s = root.find('./description/title-info/coverpage/img')
-    root.insert(0, s)
+    cover = root.find('./description/title-info/coverpage/img')
+    root.insert(0, cover)
+    # endregion
 
+    # region remove unused tags
     for el in root.findall('description'):
         root.remove(el)
     for el in root.findall('binary'):
         root.remove(el)
+    # endregion
 
+    # region change tags for html 
     for el in root.iter():
-        if el.tag in d:
-            el.tag = d.get(el.tag)
+        if el.tag in change:
+            el.tag = change.get(el.tag)
+    # endregion
 
     tree.write('output.html', encoding='utf-8')
 
