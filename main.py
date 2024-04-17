19f5b4da39bd9147a48924873d4e86887119dbe3
Natalia <natashavish643@gmail.com>
2024-04-17 22:43:17 +0300

    Fixed import warnings

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from imports_file import *
+# flask import
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager
+import os
+# database import
+from data import db_session
+from data.users import User
+from data.pass_decoder import hash_md5
+# renders' import
+from renders import epub_render, txt_render, style_render, pdf_render, fb2_render
+# raters and generators
+from placeholder_funcs import quoter as pg
+from raters.login_raters import is_correct_password, is_correct_email
+# flask-wtf import
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 # region Flask app configuration
 DEFAULT_DB = "db/data.db"
@@ -76,6 +92,8 @@ def create_folder(folder_name=UPLOAD_FOLDER):
         os.mkdir(folder_name)
     except FileExistsError:
         pass
+
+
 # endregion
 
 
@@ -191,7 +209,8 @@ def show_success_auth():
     if current_settings['is_reg']:
         return render_template('success_auth.html', greeting_phrase="Добро пожаловать!",
                                action_type='зарегестрировались', user_email=current_settings['current_email'])
-    return render_template('success_auth.html', greeting_phrase="С возвращением!", action_type='вошли в аккаунт',
+    return render_template('success_auth.html', greeting_phrase="С возвращением!",
+                           action_type='вошли в аккаунт',
                            user_email=current_settings['current_email'])
 
 
