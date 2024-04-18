810528770c52c7c38953c43c30c8e8a5afe53aa4
NVishnevskaya <140640449+NVishnevskaya@users.noreply.github.com>
2024-04-18 19:30:05 +0300

    Added main.py with launch

--- main.py
+++ main.py
@@ -247,6 +247,9 @@ def handle_file_upload():
         return redirect(url_for('show_main_page'))
 
 
-if __name__ == '__main__':
+def launch_app():
     create_folder()
     app.run(port=8080)
+
+
+launch_app()
